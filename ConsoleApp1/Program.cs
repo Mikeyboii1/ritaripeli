aa6d461a4266cfd81fdbe29afe4d07863b364a7c
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-04-13 10:21:56 +0300

    Pelaajan raha ja elämä pisteet ovat 10
    .

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Kaksi kokonaislukua (integer)
+            int playerHealth = 10;
+            int playerwealth = 10;
+
         }
     }
 }
