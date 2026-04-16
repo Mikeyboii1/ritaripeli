b87f94c29dc380992858b47065e465853618ebe0
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-04-16 12:32:31 +0300

    ohjaus

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Tervetuloa seikkailuun!");
             // Pelin alussa ritarilla on vain miekka,10 kultarahaa ja 10 osumapistettä.
             int playerHealth = 10;
-            int playerwealth = 10;
+            int playerWealth = 10;
+            Console.WriteLine($"Tilanne: Sinulla on {playerHealth} osumapistettä ja {playerWealth} kultarahaa.");
+
+            Console.WriteLine("Valitse toiminto:");
+            Console.WriteLine("1 Mene nuolikauppaan");
+            Console.WriteLine("2 Mene ravintolaan");
+            Console.WriteLine("3 Lähde taisteluun");
+            Console.WriteLine("4 Käytä repussa olevia esineitä");
 
             // Pelaaja voi vierailla kaupassa, josta voi ostaa ruokaa. Ruoka lisää osumapisteitä.
-            Console.WriteLine("Tervetuloa kauppaan! Mitä haluasit ostaa?");
         }
     }
 }
